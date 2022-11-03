9303147a19c41332ccf1b7cbe17597b183cd20fc
Ruben Villagrana <RubenVillagrana35@gmail.com>
2022-11-02 19:36:49 -0700

    Classroom work

---
+++ script.js
@@ -0,0 +1,8 @@
+//console .log (avariable)
+let myGreeting = "Hello World";
+console.log(myGreeting );
+const pi = 3.14;
+let myNumber = 50;
+console.log(myNumber);
+console.log(Math.min(myNumber, pi));
+
